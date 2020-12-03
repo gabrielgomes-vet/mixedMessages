34c36ff91e8e014391cee731f5f56762d05f86de
gabrielgomes-vet <gabrielgomes_vet@hotmail.com>
2020-12-03 09:33:41 -0500

    Adds lists

--- messages.js
+++ messages.js
@@ -1,1 +1,5 @@
 //Start here 
+const pronoun = ['My', 'Your', 'His', 'Her'];
+const noun = ['funeral', 'passion', 'attitude', 'heart', 'memory']
+const adjective = ['different', 'acid', 'fertile', 'great', 'horrible']
+const verb = ['is', 'are']
