6567cdce50aded3338027037bb97f0a5bee5d05f
gabrielgomes-vet <gabrielgomes_vet@hotmail.com>
2020-12-03 09:44:08 -0500

    Removes verbs

--- messages.js
+++ messages.js
@@ -2,4 +2,3 @@
 const pronoun = ['My', 'Your', 'His', 'Her'];
 const noun = ['funeral', 'passion', 'attitude', 'heart', 'memory']
 const adjective = ['different', 'acid', 'fertile', 'great', 'horrible']
-const verb = ['is', 'are']
